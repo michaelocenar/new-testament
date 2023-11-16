d92d61421c71995de94d9b835e063b4a8615f015
michaelocenar <affectuminflamma@gmail.com>
2023-11-16 14:26:14 -0700

    make it ok to not put comma

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 document.getElementById('submit').addEventListener('click', function() {
-  const correctBooks = ["Matthew", "Mark", "Luke", "John", "Acts", "Romans", 
-      "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians", "Philippians", 
-      "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy", "2 Timothy", 
-      "Titus", "Philemon", "Hebrews", "James", "1 Peter", "2 Peter", "1 John", 
-      "2 John", "3 John", "Jude", "Revelation"];
-  const userInput = document.getElementById('userInput').value;
-  const userBooks = userInput.split(/,|\n/).map(book => book.trim()).filter(book => book);
+  const correctBooks = ["matthew", "mark", "luke", "john", "acts", "romans", 
+      "1 corinthians", "2 corinthians", "galatians", "ephesians", "philippians", 
+      "colossians", "1 thessalonians", "2 thessalonians", "1 timothy", "2 timothy", 
+      "titus", "philemon", "hebrews", "james", "1 peter", "2 peter", "1 john", 
+      "2 john", "3 john", "jude", "revelation"];
+  const userInput = document.getElementById('userInput').value.toLowerCase();
+  
+  // Split the user input by various separators
+  const separators = [',', '\n', ' ']; // Add more separators if needed
+  const regex = new RegExp('[' + separators.join('') + ']+');
+  const userBooks = userInput.split(regex).map(book => book.trim()).filter(book => book);
 
   const correctAnswers = userBooks.filter(book => correctBooks.includes(book));
   const missedBooks = correctBooks.filter(book => !userBooks.includes(book));
